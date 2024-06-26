d732f02e41258a6f33a5588b9686a4a0ae21cc3f
gcmarcello <marcellogcoelho@outlook.com>
2024-06-26 20:53:05 +0000

    feat: user module

---
+++ apps/backend/src/resources/users/users.module.ts
@@ -0,0 +1,8 @@
+import { Module } from "@nestjs/common";
+import { UserService } from "./users.service";
+
+@Module({
+  controllers: [],
+  providers: [UserService],
+})
+export class UsersModule {}
