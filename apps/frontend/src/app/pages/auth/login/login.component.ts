5c71b437195bbeb2fe96ddae4cfafc27d95ec0d0
gcmarcello <marcellogcoelho@outlook.com>
2024-06-26 21:18:35 +0000

    feat: Add login page components and functionality

---
+++ apps/frontend/src/app/pages/auth/login/login.component.ts
@@ -0,0 +1,54 @@
+import { Component, signal } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
+import { InputComponent } from "../../../components/form/input/input.component";
+import { ButtonComponent } from "../../../components/button/button.component";
+import {
+  ClassValidatorFormBuilderModule,
+  ClassValidatorFormBuilderService,
+} from "ngx-reactive-form-class-validator";
+import { Router, RouterModule } from "@angular/router";
+import { AuthService } from "../../../services/auth.service";
+import { CheckboxComponent } from "../../../components/form/checkbox/checkbox.component";
+import { LoginDto } from "shared-types";
+
+@Component({
+  selector: "app-login",
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    InputComponent,
+    ButtonComponent,
+    ClassValidatorFormBuilderModule,
+    CheckboxComponent,
+    RouterModule,
+  ],
+  templateUrl: "./login.component.html",
+})
+export class LoginComponent {
+  profileForm = this.fb.group(
+    LoginDto,
+    {
+      email: [""],
+      password: [""],
+      remember: [false],
+    },
+    { updateOn: "change" }
+  );
+  loading = signal(false);
+
+  constructor(
+    private fb: ClassValidatorFormBuilderService,
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  async login() {
+    this.loading.set(true);
+    this.authService.login(this.profileForm).subscribe({
+      next: (result) => {
+        this.router.navigate(["/"]);
+      },
+      complete: () => this.loading.set(false),
+    });
+  }
+}
