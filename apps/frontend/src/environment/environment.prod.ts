4e70f22ed8690a07768531b69bcd6d79c8e64411
gcmarcello <marcellogcoelho@outlook.com>
2024-06-26 22:52:51 +0000

    dev: Update production API URL in environment.prod.ts

--- apps/frontend/src/environment/environment.prod.ts
+++ apps/frontend/src/environment/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: "https://jba6832a14.execute-api.us-east-1.amazonaws.com/dev",
+  apiUrl: "https://jwa92g2bxg.execute-api.us-east-1.amazonaws.com/production",
 };
