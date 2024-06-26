d985f6eb1897cc242dbb0d24de1e9f0e673e0034
gcmarcello <marcellogcoelho@outlook.com>
2024-06-26 00:38:06 +0000

    feat: dynamoose backend setup

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -2,11 +2,19 @@ import { NestFactory } from "@nestjs/core";
 import serverlessExpress from "@codegenie/serverless-express";
 import { Callback, Context, Handler } from "aws-lambda";
 import { AppModule } from "./app.module";
+import * as dynamoose from "dynamoose";
 
 let server: Handler;
 
 async function bootstrap(): Promise<Handler> {
   const app = await NestFactory.create(AppModule);
+
+  const ddb = new dynamoose.aws.ddb.DynamoDB({
+    region: "us-east-1",
+  });
+
+  dynamoose.aws.ddb.set(ddb);
+
   await app.init();
 
   const expressApp = app.getHttpAdapter().getInstance();
