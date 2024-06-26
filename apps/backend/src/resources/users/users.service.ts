f06d5c035244b00cd55a205b89072da35b46d569
gcmarcello <marcellogcoelho@outlook.com>
2024-06-26 20:52:53 +0000

    recaftor: replace dynamoose with dynamo client

--- apps/backend/src/resources/users/users.service.ts
+++ apps/backend/src/resources/users/users.service.ts
@@ -1,21 +1,32 @@
 // services/user.service.ts
 import { Injectable } from "@nestjs/common";
 import * as bcrypt from "bcrypt";
-import { UserModel, SignupDto } from "shared-types";
+import { SignupDto, User } from "shared-types";
+import { dynamo } from "src/infrastructure/database";
 
 @Injectable()
 export class UserService {
   async findUserByEmail(email: string) {
-    const users = await UserModel.query("email").eq(email).exec();
+    const users = await dynamo().query({
+      TableName: "users",
+      IndexName: "EmailIndex",
+      KeyConditionExpression: "email = :email",
+      ExpressionAttributeValues: {
+        ":email": { S: email },
+      },
+    });
 
     if (!users || users.length === 0) return null;
 
-    return users[0];
+    return users[0] as User;
   }
 
   async findUserById(id: string) {
-    const user = await UserModel.get(id, {
-      attributes: ["email", "name", "phone", "userId"],
+    const user = await dynamo().getItem({
+      TableName: "users",
+      Key: {
+        id: { S: id },
+      },
     });
 
     return user;
@@ -24,15 +35,24 @@ export class UserService {
   async createUser(data: SignupDto) {
     const hashedPassword = await bcrypt.hash(data.password, 10);
 
-    const newUser = new UserModel({
+    const newUser: User = {
       id: crypto.randomUUID(),
       email: data.email,
       name: data.name,
       password: hashedPassword,
       phone: data.phone,
-    });
+    };
 
-    await newUser.save();
+    await dynamo().putItem({
+      TableName: "users",
+      Item: {
+        id: { S: newUser.id },
+        email: { S: newUser.email },
+        name: { S: newUser.name },
+        password: { S: newUser.password },
+        phone: { S: newUser.phone },
+      },
+    });
 
     return newUser;
   }
