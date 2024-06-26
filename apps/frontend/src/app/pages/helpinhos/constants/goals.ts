0c6e5be94f1db41854302d0f697292c33888d6ce
gcmarcello <marcellogcoelho@outlook.com>
2024-06-26 21:18:12 +0000

    feat: Add constants for categories and goals in helpinhos page

---
+++ apps/frontend/src/app/pages/helpinhos/constants/goals.ts
@@ -0,0 +1,26 @@
+export const goals = [
+  {
+    name: "R$ 1.000",
+    value: 1000,
+  },
+  {
+    name: "R$ 2.000",
+    value: 2000,
+  },
+  {
+    name: "R$ 5.000",
+    value: 5000,
+  },
+  {
+    name: "R$ 10.000",
+    value: 10000,
+  },
+  {
+    name: "R$ 20.000",
+    value: 20000,
+  },
+  {
+    name: "R$ 50.000",
+    value: 50000,
+  },
+];
