a2878e221ac0996d7d82adcc9157e5e4519e2737
gcmarcello <marcellogcoelho@outlook.com>
2024-06-26 20:58:57 +0000

    feat: import modules into app

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { AuthModule } from "./resources/auth/auth.module";
+import { HelpinhoModule } from "./resources/helpinho/helpinho.module";
+import { FilesModule } from "./resources/files/files.module";
 
 @Module({
-  imports: [],
+  imports: [AuthModule, HelpinhoModule, FilesModule],
   controllers: [AppController],
   providers: [AppService],
 })
