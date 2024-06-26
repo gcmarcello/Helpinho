66e86c132ad208bb0b0c0c0b4a96913448663455
gcmarcello <marcellogcoelho@outlook.com>
2024-06-26 21:11:52 +0000

    feat: form util functions

---
+++ apps/frontend/src/app/utils/form.ts
@@ -0,0 +1,23 @@
+import { FormGroup } from "@angular/forms";
+import { EMPTY, Observable } from "rxjs";
+
+export function parseInputErrors(formGroup: FormGroup, name: string) {
+  const errors = formGroup.get(name)?.errors;
+  if (!errors) return;
+  const parsedErrors = Object.values(formGroup.get(name)?.errors!)[0];
+  return parsedErrors;
+}
+
+export function handleFormErrors(
+  form: FormGroup,
+  error: any
+): Observable<never> {
+  if (error?.error?.fields && error.error.fields.length) {
+    for (const field of error.error.fields) {
+      if (form.controls[field]) {
+        form.controls[field].setErrors({ invalid: error.error.message });
+      }
+    }
+  }
+  return EMPTY;
+}
