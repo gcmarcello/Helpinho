cbd0c62378829f303881e74f8c5a8c15c03546be
gcmarcello <marcellogcoelho@outlook.com>
2024-06-27 10:45:50 +0000

    feat: added helpinho page route

--- apps/frontend/src/app/app.routes.ts
+++ apps/frontend/src/app/app.routes.ts
@@ -1,11 +1,12 @@
 import { Routes } from "@angular/router";
 import { LoginComponent } from "./pages/auth/login/login.component";
 import { HomeComponent } from "./pages/home/home.component";
-import { HelpinhosComponent } from "./pages/helpinhos/helpinhos.component";
+import { HelpinhosComponent } from "./pages/helpinhos/novo/novo-helpinho.component";
 import { RegisterComponent } from "./pages/auth/register/register.component";
 import { NotFoundComponent } from "./pages/not-found/notfound.component";
 import { AuthComponent } from "./pages/auth/auth.component";
 import { AuthGuard, AuthPageGuard } from "./guards/auth.guard";
+import { HelpinhoComponent } from "./pages/helpinhos/helpinho/helpinho.component";
 
 export const routes: Routes = [
   { path: "", component: HomeComponent },
@@ -15,9 +16,8 @@ export const routes: Routes = [
     canActivate: [AuthGuard],
   },
   {
-    path: "helpinho/:id",
-    component: HelpinhosComponent,
-    canActivate: [AuthGuard],
+    path: "helpinhos/:id",
+    component: HelpinhoComponent,
   },
   {
     path: "auth",
