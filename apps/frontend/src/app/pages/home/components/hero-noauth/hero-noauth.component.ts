d8a86ec712d89eebd4fca2c5465b5746ba94c14d
gcmarcello <marcellogcoelho@outlook.com>
2024-06-27 10:42:54 +0000

    feat: added carousel to hero

--- apps/frontend/src/app/pages/home/components/hero-noauth/hero-noauth.component.ts
+++ apps/frontend/src/app/pages/home/components/hero-noauth/hero-noauth.component.ts
@@ -1,11 +1,17 @@
 import { Component, signal, effect, OnInit } from "@angular/core";
 import { ButtonComponent } from "../../../../components/button/button.component";
 import { CommonModule } from "@angular/common";
+import { CarouselComponent } from "../carousel/carousel.component";
 
 @Component({
   selector: "app-hero-noauth",
   standalone: true,
-  imports: [ButtonComponent, CommonModule],
+  imports: [ButtonComponent, CommonModule, CarouselComponent],
   templateUrl: "./hero-noauth.component.html",
 })
-export class HeroNoAuthComponent {}
+export class HeroNoAuthComponent {
+  numVisible = 3;
+  numScroll = 3;
+  circular = false;
+  items = ["Registro Grátis", "Sem Taxas", "Rápido e Prático"];
+}
