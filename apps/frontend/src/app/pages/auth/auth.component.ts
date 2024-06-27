567728f640c87ab627ebcd60d40459f6df06daa4
gcmarcello <marcellogcoelho@outlook.com>
2024-06-27 10:40:40 +0000

    feat: home redirect logo

--- apps/frontend/src/app/pages/auth/auth.component.ts
+++ apps/frontend/src/app/pages/auth/auth.component.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
 import { RouterModule } from "@angular/router";
 
 @Component({
   selector: "app-auth",
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: "./auth.component.html",
 })
 export class AuthComponent {}
